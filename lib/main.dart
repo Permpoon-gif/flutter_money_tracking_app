1bc659ae096db4be879b3018f06cc0045a9b209b
Permpoon-gif <paempoom1111@gmail.com>
2026-04-14 21:31:08 +0700

    3

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_money_tracking_app/views/SplashScreenUI.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+   await initializeDateFormatting('th', null); // 👈 ตัวนี้สำคัญ
+
+  
+
   await Supabase.initialize(
     url: 'https://yxofwqhqhnoywtrwxjvk.supabase.co',
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl4b2Z3cWhxaG5veXd0cnd4anZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYxNTgzNTIsImV4cCI6MjA5MTczNDM1Mn0.Tsqd6w-_9ohgo1bVgV89Mp-zaP0WXnsuUbX7ke-lN2Y',
